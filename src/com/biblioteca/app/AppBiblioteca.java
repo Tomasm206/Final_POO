31355a8aaee3aebebcee1e23bbf71ebbd8f67b7d
Tomás Marín <79349359+Tomasm206@users.noreply.github.com>
2024-07-24 19:26:24 -0500

    se actualizó la app

--- src/com/biblioteca/app/AppBiblioteca.java
+++ src/com/biblioteca/app/AppBiblioteca.java
@@ -3,9 +3,12 @@ package com.biblioteca.app;
 import com.biblioteca.dominio.Biblioteca;
 import com.biblioteca.dominio.Recurso;
 
+import java.awt.datatransfer.SystemFlavorMap;
+
 public class AppBiblioteca {
     public static void main(String[] args) {
         System.out.println("El app funciona");
         Biblioteca biblioteca = new Biblioteca();
+        System.out.println("El app no funciona");
     }
 }
